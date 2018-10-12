6544d80451ca381fd8216988b76e6dc078de7956
wjsm93@bitbucket.org <wjsm93@bitbucket.org>
2018-10-12 10:32:40 -0300

    Intent nativo iOS a Instagram

--- IntentPlugin/src/ios/IntentPlugin.swift
+++ IntentPlugin/src/ios/IntentPlugin.swift
@@ -39,4 +39,47 @@
       callbackId: command.callbackId
     )
   }
+
+  @objc(openApp:)
+  func openApp(command: CDVInvokedUrlCommand) {
+    var pluginResult = CDVPluginResult(
+      status: CDVCommandStatus_ERROR
+    )
+
+    let msg = command.arguments[0] as? String ?? ""
+
+    if msg.count > 0 {
+      
+      let instagramHooks = "instagram://app"
+      let instagramUrl = NSURL(string: instagramHooks)
+      
+      guard let url = URL(string: "https://instagram.com/") else {
+          return //be safe
+      }
+      
+      if UIApplication.shared.canOpenURL(instagramUrl! as URL) {
+        if #available(iOS 10.0, *) {
+          UIApplication.shared.open(instagramUrl! as URL, options: [:], completionHandler: nil)
+        } else {
+          UIApplication.shared.openURL(instagramUrl! as URL)
+        }
+      } else {
+        if #available(iOS 10.0, *) {
+          UIApplication.shared.open(url, options: [:], completionHandler: nil)
+        } else {
+          UIApplication.shared.openURL(url)
+        }
+      }
+        
+      pluginResult = CDVPluginResult(
+        status: CDVCommandStatus_OK,
+        messageAs: msg
+      )
+    }
+
+    self.commandDelegate!.send(
+      pluginResult,
+      callbackId: command.callbackId
+    )
+  }
 }
--- IntentPlugin/www/IntentPlugin.js
+++ IntentPlugin/www/IntentPlugin.js
@@ -3,3 +3,7 @@ var exec = require('cordova/exec');
 exports.echo = function(arg0, success, error) {
   exec(success, error, 'IntentPlugin', 'echo', [arg0]);
 };
+
+exports.openApp = function(arg0, success, error) {
+  exec(success, error, 'IntentPlugin', 'openApp', [arg0]);
+};
