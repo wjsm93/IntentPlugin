dcd2d6dec385cd423c00dbcb083428b15ff70201
William Sandoval <wsandoval@MacBook-Pro-de-William.local>
2018-10-16 16:12:10 -0300

    Intent a OnePay app iOS

--- IntentPlugin/src/ios/IntentPlugin.swift
+++ IntentPlugin/src/ios/IntentPlugin.swift
@@ -1,45 +1,4 @@
 @objc(IntentPlugin) class IntentPlugin : CDVPlugin {
-  @objc(echo:)
-  func echo(command: CDVInvokedUrlCommand) {
-    var pluginResult = CDVPluginResult(
-      status: CDVCommandStatus_ERROR
-    )
-
-    let msg = command.arguments[0] as? String ?? ""
-
-    if msg.count > 0 {
-      let toastController: UIAlertController =
-        UIAlertController(
-          title: "",
-          message: msg,
-          preferredStyle: .alert
-        )
-      
-      self.viewController?.present(
-        toastController,
-        animated: true,
-        completion: nil
-      )
-
-      DispatchQueue.main.asyncAfter(deadline: .now() + 3) {
-        toastController.dismiss(
-          animated: true,
-          completion: nil
-        )
-      }
-        
-      pluginResult = CDVPluginResult(
-        status: CDVCommandStatus_OK,
-        messageAs: msg
-      )
-    }
-
-    self.commandDelegate!.send(
-      pluginResult,
-      callbackId: command.callbackId
-    )
-  }
-
   @objc(openApp:)
   func openApp(command: CDVInvokedUrlCommand) {
     var pluginResult = CDVPluginResult(
@@ -49,21 +8,31 @@
     let msg = command.arguments[0] as? String ?? ""
 
     if msg.count > 0 {
+
+      let onePayScheme = "onepay://"
+      let onePayAppStore = "https://itunes.apple.com/cl/app/onepay/id1218407961?mt=8"
       
-      let instagramHooks = "instagram://app"
-      let instagramUrl = NSURL(string: instagramHooks)
+      let onePayApp = NSURL(string: onePayScheme)
       
-      guard let url = URL(string: "https://instagram.com/") else {
-          return //be safe
+      // Validar que la url esté bien formulada
+      guard let url = URL(string: onePayAppStore) else {
+        return //be safe
       }
+
+      var components = URLComponents(string: onePayScheme)
+
+      var comQueryItems: [AnyHashable] = []
+      comQueryItems.append(URLQueryItem(name: "occ", value: "1810734958644939"))
+
+      components?.queryItems = comQueryItems as? [URLQueryItem]
       
-      if UIApplication.shared.canOpenURL(instagramUrl! as URL) {
-        if #available(iOS 10.0, *) {
-          UIApplication.shared.open(instagramUrl! as URL, options: [:], completionHandler: nil)
-        } else {
-          UIApplication.shared.openURL(instagramUrl! as URL)
+      // Si la app está instalada
+      if UIApplication.shared.canOpenURL(onePayApp! as URL) {
+        if let anURL = components?.url {
+            UIApplication.shared.openURL(anURL)å
         }
       } else {
+        // Si la app no está instalada, abrir enlace de AppStore
         if #available(iOS 10.0, *) {
           UIApplication.shared.open(url, options: [:], completionHandler: nil)
         } else {
--- IntentPlugin/www/IntentPlugin.js
+++ IntentPlugin/www/IntentPlugin.js
@@ -1,9 +1,5 @@
 var exec = require('cordova/exec');
 
-exports.echo = function(arg0, success, error) {
-  exec(success, error, 'IntentPlugin', 'echo', [arg0]);
-};
-
 exports.openApp = function(arg0, success, error) {
   exec(success, error, 'IntentPlugin', 'openApp', [arg0]);
 };
