aad068eed93d70ff682969d216ec64ca863fbbb1
William Sandoval <wjsm93@bitbucket.org>
2018-10-18 15:29:36 -0300

    Fixes para integración con OutSystems

--- IntentPlugin/www/IntentPlugin.js
+++ IntentPlugin/www/IntentPlugin.js
@@ -1,5 +1,14 @@
-var exec = require('cordova/exec');
+var intentplugin = {
+  openApp: function (arg0, success, error) {
+    exec(success, error, 'IntentPlugin', 'openApp', [arg0]);
+  }
+}
 
-exports.openApp = function(arg0, success, error) {
-  exec(success, error, 'IntentPlugin', 'openApp', [arg0]);
-};
+cordova.addConstructor(function () {
+  if (!window.plugins) {
+    window.plugins = {};
+  }
+
+  window.plugins.intentplugin = intentplugin;
+  return window.plugins.intentplugin;
+});
